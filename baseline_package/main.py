5337d06260cf7b0a06ad30ed120b3e39015dfa0e
Wolf <wolf@tgwolf.com>
2024-05-16 15:55:27 +0100

    Fix pipeline issues

--- baseline_package/main.py
+++ baseline_package/main.py
@@ -5,7 +5,6 @@ you can elaborate further on details as appropriate for the situation.
 Notice that the summary and the elaboration is separated by a blank new
 line.
 """
-
 import sys
 
 from .cli import run
